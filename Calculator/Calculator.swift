e547084c1e68f2a16f105a61cffa6a4ad399ec35
UCode <ucode@ucode-tpv-zeta.fios-router.home>
2018-05-06 13:01:05 -0700

    started the code

---
+++ Calculator/Calculator.swift
@@ -0,0 +1,29 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by UCode on 5/6/18.
+//  Copyright © 2018 Anjali. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+   
+    
+    var input : Double
+    var output : Double
+    var operation : Operation
+    var decimalPlace : Int
+    
+    init() {
+        
+    }
+    
+    func digitPressed(digit : Int) {
+        var AC = input * 0
+    }
+    
+    
+    
+}
